9bcdb41dcce0dcc889895276bb51b35f9877877e
James <winleiei@Wins-MacBook-Air.local>
2025-01-04 21:30:33 +0630

    Finalize the app

---
+++ lib/appwrite.ts
@@ -0,0 +1,155 @@
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  OAuthProvider,
+  Query,
+} from "react-native-appwrite";
+import { openAuthSessionAsync } from "expo-web-browser";
+import * as Linking from "expo-linking";
+
+export const config = {
+  plaform: "com.nhl.restate",
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID,
+  galleriesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_GALLERIES_COLLECTION_ID,
+  reviewsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_REVIEWS_COLLECTION_ID,
+  agentsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_AGENT_COLLECTION_ID,
+  propertiesCollectionId:
+    process.env.EXPO_PUBLIC_APPWRITE_PROPERTIES_COLLECTION_ID,
+};
+export const client = new Client();
+
+client
+  .setEndpoint(config.endpoint!)
+  .setProject(config.projectId!)
+  .setPlatform(config.plaform!);
+
+export const avatar = new Avatars(client);
+export const account = new Account(client);
+export const databases = new Databases(client);
+
+export async function login() {
+  try {
+    const redirectUri = Linking.createURL("/");
+    const response = await account.createOAuth2Token(
+      OAuthProvider.Google,
+      redirectUri
+    );
+    if (!response) throw new Error("OAuth2 token creation failed");
+
+    const browserResult = await openAuthSessionAsync(
+      response.toString(),
+      redirectUri
+    );
+    if (browserResult.type !== "success")
+      throw new Error("OAuth session failed");
+
+    const url = new URL(browserResult.url);
+    const secret = url.searchParams.get("secret");
+    const userId = url.searchParams.get("userId");
+
+    if (!secret || !userId) throw new Error("Invalid OAuth session data");
+
+    const session = await account.createSession(userId, secret);
+    if (!session) throw new Error("Session creation failed");
+
+    console.log("User logged in:", session);
+    return session;
+  } catch (error) {
+    console.error("Login error:", error);
+    return false;
+  }
+}
+
+export async function logout() {
+  try {
+    await account.deleteSession("current");
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+export async function getCurrentUser() {
+  try {
+    const response = await account.get();
+    if (response.$id) {
+      const userAvatar = avatar.getInitials(response.name);
+      return {
+        ...response,
+        avatar: userAvatar.toString(),
+      };
+    }
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getLatestProperties() {
+  try {
+    const result = await databases.listDocuments(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      [Query.orderAsc("$createdAt"), Query.limit(5)]
+    );
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+export async function getProperties({
+  filter,
+  query,
+  limit,
+}: {
+  filter: string;
+  query: string;
+  limit?: number;
+}) {
+  try {
+    const buildQuery = [Query.orderDesc("$createdAt")];
+    if (filter && filter !== "All") {
+      buildQuery.push(Query.equal("type", filter));
+    }
+    if (query) {
+      buildQuery.push(
+        Query.or([
+          Query.search("name", query),
+          Query.search("address", query),
+          Query.search("type", query),
+        ])
+      );
+    }
+    if (limit) buildQuery.push(Query.limit(limit));
+    const result = await databases.listDocuments(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      buildQuery
+    );
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+// write function to get property by id
+export async function getPropertyById({ id }: { id: string }) {
+  try {
+    const result = await databases.getDocument(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      id
+    );
+    return result;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
